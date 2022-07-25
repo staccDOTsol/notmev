b6d20e86c86c46da5b68345f2a7e874d3c883f50
jarett dunn <jarettdunn@jaretts-MacBook-Air.local>
2022-07-24 22:09:36 -0300

    bla

--- somejson.py
+++ somejson.py
@@ -12,16 +12,15 @@ for name in glob.glob('./ha/*.json'):
         with open (name, 'r') as f:
             bla = json.loads(f.read())
         for b in bla:
-            if len(b['transaction']['message']['accountKeys']) >= 20:
+            if len(b['transaction']['message']['accountKeys']) == 23 :
                 if b['transaction']['message']['accountKeys'][19] == "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin":
-                    if ((b['transaction']['message']['accountKeys'][17]) not in someshit):
-                        try:
-                            someshit[b['transaction']['message']['accountKeys'][17]].append (b['transaction']['message']['accountKeys'][12])
-                        except:
-                            someshit[b['transaction']['message']['accountKeys'][17]] = [b['transaction']['message']['accountKeys'][12]]
-                            someshit[b['transaction']['message']['accountKeys'][17]].append (b['transaction']['message']['accountKeys'][12])
-                        print(len(someshit))
-                        with open ('someshit.json', 'w') as f:
-                            f.write(json.dumps(someshit))
+                    try:
+                        someshit[b['transaction']['message']['accountKeys'][17]].append (b['transaction']['message']['accountKeys'][12])
+                    except:
+                        someshit[b['transaction']['message']['accountKeys'][17]] = [b['transaction']['message']['accountKeys'][12]]
+                    print(len([b['transaction']['message']['accountKeys'][17]]))
+                    with open ('someshit.json', 'w') as f:
+                        f.write(json.dumps(someshit))
+                    print( len(b['transaction']['message']['accountKeys']))
     except:
         print('blah')
